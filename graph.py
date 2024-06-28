1c2b13061e0db4c37fa2c35f4636093e6a1c980b
nanaba <102130459+nanataste@users.noreply.github.com>
2024-06-28 13:50:40 +0900

    Update graph.py
    plt 2개 인거 제거

--- graph.py
+++ graph.py
@@ -16,9 +16,6 @@
  plt.show()
 
 
- import matplotlib.pyplot as plt
-
-
  X = [ "Mon", "Tue", "Wed", "Thur", "Fri",  "Sat", "Sun" ] 
  plt.plot(X, [15.6, 14.2, 16.3, 18.2, 17.1, 20.2, 22.4], "sm")
  plt.show()
